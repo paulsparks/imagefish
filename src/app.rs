414f6892bd51af7a2c0dc00512831c01d3202e81
Paul Sparks <psparks1225@gmail.com>
2025-02-09 17:05:38 -0500

    version 1

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,15 @@
-use eframe::egui;
+use std::{
+    fs, io::Write, path::PathBuf
+};
 
 #[derive(Default)]
-pub struct ImageFish {}
+pub struct ImageFish {
+    files: Option<Vec<PathBuf>>,
+    destination: Option<PathBuf>,
+}
 
 impl ImageFish {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
@@ -13,10 +18,45 @@ impl ImageFish {
     }
 }
 
+
 impl eframe::App for ImageFish {
-   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-           ui.heading("Hello World!");
-       });
-   }
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // ui.heading("Image Fish");
+
+            if ui.button("Select Images").clicked() {
+                self.files = rfd::FileDialog::new()
+                    .add_filter("image", &["jpg", "jpeg", "png"])
+                    .set_directory("/")
+                    .pick_files();  
+            }
+
+            if ui.button("Select Destination").clicked() {
+                self.destination = rfd::FileDialog::new().pick_folder();
+            }
+
+            if ui.button("Export All").clicked() {
+                let folder = self.destination.clone().unwrap();
+
+                for file in self.files.clone().unwrap() {
+                    let filename = rfd::FileHandle::from(file.clone()).file_name();
+                    let file = img_to_64_px(&file).unwrap();
+
+                    raster::save(&file, &folder.join(filename).to_str().unwrap()).unwrap();
+                }
+            }
+        });
+    }
+}
+
+fn img_to_64_px(img: &PathBuf) -> Result<raster::Image, raster::error::RasterError> {
+    let mut i = raster::open(img.to_str().unwrap())?;
+
+    if i.width <= i.height {
+        raster::editor::resize(&mut i, 64, 64, raster::ResizeMode::ExactWidth)?;
+    } else {
+        raster::editor::resize(&mut i, 64, 64, raster::ResizeMode::ExactHeight)?;
+    }
+
+    Ok(i)
 }
